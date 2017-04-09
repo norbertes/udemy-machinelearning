ad37b150cf36f1a45a91365180563ecaf6841379
Norbert Sienkiewicz <norbert.sienkiewicz@gmail.com>
2017-04-09 16:59:44 +0200

    checkpoint after lecture 12

---
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -0,0 +1,20 @@
+# Data Preprocessing Template
+dataset = read.csv('./Data.csv')
+
+# Take care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country,
+                         levels=c('France', 'Spain', 'Germany'),
+                         labels=c(1, 2, 3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels=c('Yes', 'No'),
+                           labels=c(1, 0))
